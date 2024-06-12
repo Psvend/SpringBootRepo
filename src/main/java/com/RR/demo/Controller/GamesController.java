05a14b6f90714ca52b8ee8c3f435b5e0e6c59bf1
Natali K <113907659+misaki555@users.noreply.github.com>
2024-06-12 12:17:46 +0200

    Basic structure

--- src/main/java/com/RR/demo/Controller/GamesController.java
+++ src/main/java/com/RR/demo/Controller/GamesController.java
@@ -1,4 +1,19 @@
 package com.RR.demo.Controller;
 
+import com.RR.demo.Model.Games;
+import com.RR.demo.Service.GamesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GamesController {
+    @Autowired
+    private GamesService gamesService;
+
+    @PostMapping("/addGame")
+    public Games postGame(@RequestBody Games game){
+        return gamesService.saveGame(game);
+    }
 }
--- src/main/java/com/RR/demo/Repository/GamesRepo.java
+++ src/main/java/com/RR/demo/Repository/GamesRepo.java
@@ -1,4 +1,7 @@
 package com.RR.demo.Repository;
 
-public interface GamesRepo {
+import com.RR.demo.Model.Games;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GamesRepo extends JpaRepository<Games, Integer> {
 }
