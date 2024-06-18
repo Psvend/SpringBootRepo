93c597b26a203731cca5ea45d6050e2af8ceca06
Petrine <s235064@dtu.dk>
2024-06-18 11:25:07 +0200

    Register controller, repo, service added

--- src/main/java/com/RR/demo/Controller/PlayersController.java
+++ src/main/java/com/RR/demo/Controller/PlayersController.java
@@ -1,25 +1,12 @@
 package com.RR.demo.Controller;
 
-import com.RR.demo.Service.GamesService;
-import com.RR.demo.Service.PlayersService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/players")
 public class PlayersController {
-   /* @Autowired
-    private PlayersService playersService;
 
-    @PostMapping("/{playerId}/ready")
-    public ResponseEntity<String> playerReady(@PathVariable int player_id) {
-        playersService.playerReady(player_id);
-        return ResponseEntity.ok("Player marked as ready");
-    }*/
 
 
 }
