18bcc15ad615969086dc1e39edf8c7df4fc34097
Petrine <s235064@dtu.dk>
2024-06-18 14:42:19 +0200

    comments added

--- src/main/java/com/RR/demo/Repository/GamesRepo.java
+++ src/main/java/com/RR/demo/Repository/GamesRepo.java
@@ -11,16 +11,18 @@ import java.util.List;
 
 public interface GamesRepo extends JpaRepository<Games, Integer> {
 
-    //Costume Query
+
     //Find by game_status
     @Query(value = "SELECT all FROM Games all WHERE game_status=:status")
     public List<Games> findByGameStatus(@Param("status") int game_status);
 
+    //opdates the amount of joined players by one every time a player joins the lobby
     @Transactional
     @Modifying
     @Query(value = "UPDATE Games SET joined_players = joined_players + 1 WHERE game_id=:game_id", nativeQuery = true)
     public void updateJoinedAmount(@Param("game_id") int game_id);
 
+    //finds amount of joined players in a specific game specified by client
     @Query(value = "SELECT joined_players FROM Games WHERE game_id=:game_id")
     public int getJoinedPlayers(@Param("game_id") int game_id);
 }
