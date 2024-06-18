bc88733c476bea45ab23da4b926636bff2fbf434
Petrine <s235064@dtu.dk>
2024-06-18 13:23:16 +0200

    addPlayer virker!

--- src/main/java/com/RR/demo/Repository/GamesRepo.java
+++ src/main/java/com/RR/demo/Repository/GamesRepo.java
@@ -1,7 +1,9 @@
 package com.RR.demo.Repository;
 
 import com.RR.demo.Model.Games;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,7 +16,9 @@ public interface GamesRepo extends JpaRepository<Games, Integer> {
     @Query(value = "SELECT all FROM Games all WHERE game_status=:status")
     public List<Games> findByGameStatus(@Param("status") int game_status);
 
-    @Query(value = "UPDATE Games SET joined_amount = joined_amount + 1 WHERE game_id=:game_id", nativeQuery = true)
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE Games SET joined_players = joined_players + 1 WHERE game_id=:game_id", nativeQuery = true)
     public void updateJoinedAmount(@Param("game_id") int game_id);
 
 
